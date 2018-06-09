b29c7ca4d11735d4a6fc76647a614aa3e29a94f8
joncloud <jdberube@gmail.com>
2018-06-08 17:54:42 -0700

    Creates empty test project

---
+++ tests/XUnitTestProject1/IntegrationTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class IntegrationTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
