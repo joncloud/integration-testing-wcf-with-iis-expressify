d79c135c255ac788b976a69ec04391a7b676f16a
joncloud <jdberube@gmail.com>
2018-06-08 18:00:37 -0700

    Adds integration tests

--- tests/XUnitTestProject1/IntegrationTest1.cs
+++ tests/XUnitTestProject1/IntegrationTest1.cs
@@ -1,14 +1,56 @@
 using System;
+using System.ServiceModel;
+using WcfService1;
 using Xunit;
 
 namespace XUnitTestProject1
 {
+    [Collection("IIS")]
     public class IntegrationTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        readonly IisFixture _iisFixture;
+        public IntegrationTest1(IisFixture iisFixture) => _iisFixture = iisFixture;
 
+        [Fact]
+        public void GetData_ShouldEchoInputValue()
+        {
+            var value = 123;
+            var expected = $"You entered: {value}";
+
+            var actual = _iisFixture.WcfClient<IService1>("Service1.svc")
+                .BasicHttpClient()
+                .Use(svc => svc.GetData(value));
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetDataUsingDataContract_ShouldThrowGivenNullComposite()
+        {
+            _iisFixture.WcfClient<IService1>("Service1.svc")
+                .BasicHttpClient()
+                .Do(AssertService);
+
+            void AssertService(IService1 service)
+            {
+                Assert.Throws<FaultException>(() => service.GetDataUsingDataContract(null));
+            }
+        }
+
+        [Fact]
+        public void GetDataUsingDataContract_ShouldSuffixStringValue()
+        {
+            var composite = new CompositeType
+            {
+                BoolValue = true,
+                StringValue = "abc"
+            };
+            var actual = _iisFixture.WcfClient<IService1>("Service1.svc")
+                .BasicHttpClient()
+                .Use(svc => svc.GetDataUsingDataContract(composite));
+
+            Assert.Equal(composite.BoolValue, actual.BoolValue);
+            Assert.Equal(composite.StringValue + "Suffix", actual.StringValue);
         }
     }
 }
